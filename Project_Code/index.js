d4825bb325a7258a3993bdcc6c7144b9907b474a
Trevor Reed <trre4109@colorado.edu>
2022-11-10 15:47:38 -0700

    Added distinction between invalid user or invalid password

--- Project_Code/index.js
+++ Project_Code/index.js
@@ -78,13 +78,14 @@ app.post('/login', async (req, res) =>{
     } else {
       res.render('pages/login', {
         error: 'Invalid username or password',
+	message: `Wrong password, please try again`,
       });
     }
   }
   else {
     res.render('pages/login', {
       error: 'Invalid username or password',
-      message: `Wrong password, please try again`,
+      message: `Invalid username, please try again`,
     });
   }
 
