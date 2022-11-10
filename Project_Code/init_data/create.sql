4a4e03c0a27d233021cb98a27e837024bb3e51a7
Connor Rauscher <cora4829@colorado.edu>
2022-11-09 21:08:41 -0700

    made login/registration pages look better, also modified how primary key works in db

--- Project_Code/init_data/create.sql
+++ Project_Code/init_data/create.sql
@@ -4,13 +4,22 @@ CREATE TABLE users(
 );
 
 CREATE TABLE courses(
-    course_id NUMERIC PRIMARY KEY,
-    course_prefix VARCHAR(100) NOT NULL,
+    course_id INTEGER NOT NULL,
+    course_prefix VARCHAR(4) NOT NULL,
     course_name VARCHAR(100) NOT NULL,
-    credit_hours NUMERIC NOT NULL
+    credit_hours INTEGER NOT NULL
 );
 
+-- add constraint to make sure that the course_id and course_prefix are unique
+ALTER TABLE courses ADD CONSTRAINT unique_course UNIQUE (course_id, course_prefix);
+ALTER TABLE courses ADD CONSTRAINT courses_pk PRIMARY KEY (course_id, course_prefix);
+    
 CREATE TABLE user_courses(
-    course_id INTEGER NOT NULL REFERENCES courses (course_id),
-    username VARCHAR(50) NOT NULL REFERENCES users (username)
+    --get course_prefix and course_id from courses table
+    course_id INTEGER NOT NULL,
+    course_prefix VARCHAR(4) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+
+    FOREIGN KEY (course_id, course_prefix) REFERENCES courses (course_id, course_prefix),
+    FOREIGN KEY (username) REFERENCES users (username)
 );
--- Project_Code/init_data/insert.sql
+++ Project_Code/init_data/insert.sql
@@ -1,8 +1,9 @@
 INSERT INTO users (username, password) VALUES 
-('user', 'password');
+('user', '$2b$10$c57OUAmg7GF3JYAZq2K8ROZydmi8ZC1M0UAvszDa25BwvlOULVaHa'), --username is user, password is password
+('', '1');
 
 INSERT INTO courses (course_id, course_prefix, course_name, credit_hours) VALUES 
 (1000, 'CSCI', 'Intro to Computer Science', 3);
 
-INSERT INTO user_courses (username, course_id) VALUES 
-('user', 1000);
+INSERT INTO user_courses (username, course_prefix, course_id) VALUES 
+('user', 'CSCI', 1000);
