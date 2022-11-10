ae3f919cf9cc82a044f8694fbc62c713dd7f4952
Connor Rauscher <cora4829@colorado.edu>
2022-11-09 22:09:09 -0700

    Created courses.ejs, implemented logic for getting courses from database

--- Project_Code/init_data/insert.sql
+++ Project_Code/init_data/insert.sql
@@ -1,9 +1,10 @@
 INSERT INTO users (username, password) VALUES 
-('user', '$2b$10$c57OUAmg7GF3JYAZq2K8ROZydmi8ZC1M0UAvszDa25BwvlOULVaHa'), --username is user, password is password
-('', '1');
+('asd', '$2b$10$X9I23OjqzTuXLV4WwaGnWepuR0myejQycSlg0sBcPPRKAVi2hKUzq'); --username is asd, password is asd
 
 INSERT INTO courses (course_id, course_prefix, course_name, credit_hours) VALUES 
-(1000, 'CSCI', 'Intro to Computer Science', 3);
+(1000, 'CSCI', 'Intro to Computer Science', 3),
+(1000, 'ASEN', 'Aerospace Something', 4),
+(2012, 'TEST', 'Test Course 3', 1);
 
 INSERT INTO user_courses (username, course_prefix, course_id) VALUES 
-('user', 'CSCI', 1000);
+('asd', 'CSCI', 1000);
